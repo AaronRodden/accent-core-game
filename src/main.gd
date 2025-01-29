3e60da034549495fb511a74ea75220e5a852c475
Aaron Rodden <aaron.rodden21@gmail.com>
2025-01-28 18:23:12 -0800

    Preparing zelda 1 map chunk for 1st playtest

--- src/main.gd
+++ src/main.gd
@@ -1,6 +1,6 @@
 extends Node
 
-@onready var starting_overworld_chunk : OverworldChunk = $WorldNode/OverworldChunk2B
+@onready var starting_overworld_chunk : OverworldChunk = $WorldNode/OverworldChunk3C
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
