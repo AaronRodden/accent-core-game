45cadb219d1840383ce92b840f70338aa5f7d9ff
Aaron Rodden <aaron.rodden21@gmail.com>
2025-04-16 13:31:27 -0700

    changing disconnect call to hopefully remove double counting

--- scenes/racing/thought_path_racing.gd
+++ scenes/racing/thought_path_racing.gd
@@ -69,7 +69,7 @@ func _racing_instructions_input_event(event: InputEventKey, keystroke : String, 
 			
 func _start_racing():
 	instructions_sprite.visible = false
-	SignalBus.disconnect("player_keystroke", _racing_instructions_input_event)
+	SignalBus.racing_complete.disconnect(_racing_instructions_input_event)
 	
 	$CanvasLayer.visible = true
 	$OverworldChunk/Player.initalize($OverworldChunk, "racing")
